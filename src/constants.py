c386fbbb1bf90640892e3793d0630e00a9113b63
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2024-03-26 15:03:07 -0300

    Engineering the prompt for LlaMa 2

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,9 @@
 import logging
 
-CLIENT_ERROR_MSG = "No Wind River/Kubernetes API capable of answering your question was found!\nPleasy try again with another prompt."
+CLIENT_ERROR_MSG = "No Wind River/Kubernetes API capable of answering your question was found!\nPleasy try again with another prompt."  # noqa: E501
 
 LOG = logging.getLogger("chatbot")
 
-MODEL = 'meta.llama2-13b-chat-v1'
+MODEL = 'meta.llama2-70b-chat-v1'
+
+LLAMA_PROMPT_END = "Do not acknowledge my request with 'sure' or in any other way besides going straight to the answer. Do not respond with anything more than the answer. Guarantee that the format is followed."  # noqa: E501
